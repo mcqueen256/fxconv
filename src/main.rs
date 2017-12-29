f264872953715b08c61b51556e3f4d3cf0d0a62f
Nicholas Buckeridge <bucknich@gmail.com>
2017-12-29 16:44:45 +1000

    added a currently useless DataSet struct

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#[macro_use(c)]
+extern crate cute;
+
+#[derive(Debug)]
+struct DataSet {
+    names: Vec<String>,
+    columns: Vec<Vec<f64>>,
+    width: usize,
+    precision: usize
+}
+
+impl DataSet {
+    fn new(names: Vec<&str>) -> DataSet {
+        let names = c![String::from(n), for n in names.into_iter()];
+        let columns = c![Vec::new(), for _n in names.iter()];
+        DataSet { names: names, columns: columns, width: 10, precision: 5 }
+        // TODO: Change the width to a cmd line variable, same with precision
+    }
+
+
+}
+
+use std::fmt;
+
+impl fmt::Display for DataSet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let header: String = c![format!("{:<10}", n), for n in self.names.iter()].join(" "); // TODO: change the join to a variable argument --header-delimeter
+
+        write!(f, "{}", header)
+    }
+}
+
+fn main() {
+    let mut ds = DataSet::new(vec!["this", "that", "the", "other"]);
+    println!("ds:\n{}", ds);
+}
